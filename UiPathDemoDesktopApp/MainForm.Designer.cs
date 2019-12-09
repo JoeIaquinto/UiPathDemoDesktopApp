92cb401bd80d928d1acc8fd41bcf9661a07e9e04
Joe Iaquinto <joseph.iaquinto@troyweb.com>
2019-12-09 17:19:26 -0500

    Add project files.

---
+++ UiPathDemoDesktopApp/MainForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace UiPathDemoDesktopApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fnameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lnameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pwBox = new System.Windows.Forms.MaskedTextBox();
+            this.cpwBox = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.spamBox = new System.Windows.Forms.CheckBox();
+            this.tosBox = new System.Windows.Forms.CheckBox();
+            this.submitbtn = new System.Windows.Forms.Button();
+            this.randomizer = new System.Windows.Forms.Button();
+            this.dobPicker = new System.Windows.Forms.TextBox();
+            this.easybutton = new System.Windows.Forms.RadioButton();
+            this.mediumbutton = new System.Windows.Forms.RadioButton();
+            this.hardbutton = new System.Windows.Forms.RadioButton();
+            this.favNumBox = new System.Windows.Forms.TextBox();
+            // 
+            // fnameBox
+            // 
+            this.fnameBox.Location = new System.Drawing.Point(115, 16);
+            this.fnameBox.Name = "fnameBox";
+            this.fnameBox.Size = new System.Drawing.Size(208, 23);
+            this.fnameBox.TabIndex = 0;
+            this.fnameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lnameBox
+            // 
+            this.lnameBox.Location = new System.Drawing.Point(115, 45);
+            this.lnameBox.Name = "lnameBox";
+            this.lnameBox.Size = new System.Drawing.Size(208, 23);
+            this.lnameBox.TabIndex = 0;
+            this.lnameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(-184, -66);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Password";
+            // 
+            // pwBox
+            // 
+            this.pwBox.Location = new System.Drawing.Point(115, 74);
+            this.pwBox.Name = "pwBox";
+            this.pwBox.PasswordChar = '*';
+            this.pwBox.Size = new System.Drawing.Size(208, 23);
+            this.pwBox.TabIndex = 4;
+            this.pwBox.UseSystemPasswordChar = true;
+            // 
+            // cpwBox
+            // 
+            this.cpwBox.Location = new System.Drawing.Point(115, 103);
+            this.cpwBox.Name = "cpwBox";
+            this.cpwBox.Size = new System.Drawing.Size(208, 23);
+            this.cpwBox.TabIndex = 4;
+            this.cpwBox.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Confirm Password";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Date Of Birth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Favorite Number";
+            // 
+            // spamBox
+            // 
+            this.spamBox.AutoSize = true;
+            this.spamBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.spamBox.Checked = true;
+            this.spamBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.spamBox.Location = new System.Drawing.Point(79, 190);
+            this.spamBox.Name = "spamBox";
+            this.spamBox.Size = new System.Drawing.Size(244, 19);
+            this.spamBox.TabIndex = 10;
+            this.spamBox.Text = "Would you like to subscribe to our spam?";
+            this.spamBox.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.spamBox.UseVisualStyleBackColor = true;
+            // 
+            // tosBox
+            // 
+            this.tosBox.AutoSize = true;
+            this.tosBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tosBox.Location = new System.Drawing.Point(110, 215);
+            this.tosBox.Name = "tosBox";
+            this.tosBox.Size = new System.Drawing.Size(213, 19);
+            this.tosBox.TabIndex = 10;
+            this.tosBox.Text = "Do you accept the terms of service?";
+            this.tosBox.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.tosBox.UseVisualStyleBackColor = true;
+            // 
+            // submitbtn
+            // 
+            this.submitbtn.Location = new System.Drawing.Point(248, 240);
+            this.submitbtn.Name = "submitbtn";
+            this.submitbtn.Size = new System.Drawing.Size(75, 23);
+            this.submitbtn.TabIndex = 11;
+            this.submitbtn.Text = "Submit";
+            this.submitbtn.UseVisualStyleBackColor = true;
+            this.submitbtn.Click += new System.EventHandler(this.submitbtn_Click);
+            // 
+            // randomizer
+            // 
+            this.randomizer.ForeColor = System.Drawing.Color.Red;
+            this.randomizer.Location = new System.Drawing.Point(15, 293);
+            this.randomizer.Name = "randomizer";
+            this.randomizer.Size = new System.Drawing.Size(85, 23);
+            this.randomizer.TabIndex = 12;
+            this.randomizer.Text = "Randomize!";
+            this.randomizer.UseVisualStyleBackColor = true;
+            this.randomizer.Click += new System.EventHandler(this.randomizer_Click);
+            // 
+            // dobPicker
+            // 
+            this.dobPicker.Location = new System.Drawing.Point(115, 132);
+            this.dobPicker.Name = "dobPicker";
+            this.dobPicker.Size = new System.Drawing.Size(208, 23);
+            this.dobPicker.TabIndex = 0;
+            this.dobPicker.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // easybutton
+            // 
+            this.easybutton.AutoSize = true;
+            this.easybutton.Location = new System.Drawing.Point(15, 215);
+            this.easybutton.Name = "easybutton";
+            this.easybutton.Size = new System.Drawing.Size(48, 19);
+            this.easybutton.TabIndex = 13;
+            this.easybutton.TabStop = true;
+            this.easybutton.Text = "Easy";
+            this.easybutton.UseVisualStyleBackColor = true;
+            this.easybutton.CheckedChanged += new System.EventHandler(this.easybutton_CheckedChanged);
+
+            // 
+            // mediumbutton
+            // 
+            this.mediumbutton.AutoSize = true;
+            this.mediumbutton.Location = new System.Drawing.Point(15, 240);
+            this.mediumbutton.Name = "mediumbutton";
+            this.mediumbutton.Size = new System.Drawing.Size(70, 19);
+            this.mediumbutton.TabIndex = 13;
+            this.mediumbutton.TabStop = true;
+            this.mediumbutton.Text = "Medium";
+            this.mediumbutton.UseVisualStyleBackColor = true;
+            this.mediumbutton.CheckedChanged += new System.EventHandler(this.mediumbutton_CheckedChanged);
+            // 
+            // hardbutton
+            // 
+            this.hardbutton.AutoSize = true;
+            this.hardbutton.Location = new System.Drawing.Point(15, 265);
+            this.hardbutton.Name = "hardbutton";
+            this.hardbutton.Size = new System.Drawing.Size(51, 19);
+            this.hardbutton.TabIndex = 13;
+            this.hardbutton.TabStop = true;
+            this.hardbutton.Text = "Hard";
+            this.hardbutton.UseVisualStyleBackColor = true;
+            this.hardbutton.CheckedChanged += new System.EventHandler(this.hardbutton_CheckedChanged);
+            // 
+            // favNumBox
+            // 
+            this.favNumBox.Location = new System.Drawing.Point(115, 164);
+            this.favNumBox.Name = "favNumBox";
+            this.favNumBox.Size = new System.Drawing.Size(208, 23);
+            this.favNumBox.TabIndex = 0;
+            this.favNumBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 324);
+            this.Controls.Add(this.easybutton);
+            this.Controls.Add(this.randomizer);
+            this.Controls.Add(this.submitbtn);
+            this.Controls.Add(this.spamBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fnameBox);
+            this.Controls.Add(this.lnameBox);
+            this.Controls.Add(this.pwBox);
+            this.Controls.Add(this.cpwBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tosBox);
+            this.Controls.Add(this.dobPicker);
+            this.Controls.Add(this.mediumbutton);
+            this.Controls.Add(this.hardbutton);
+            this.Controls.Add(this.favNumBox);
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fnameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox lnameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox pwBox;
+        private System.Windows.Forms.MaskedTextBox cpwBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox spamBox;
+        private System.Windows.Forms.CheckBox tosBox;
+        private System.Windows.Forms.Button submitbtn;
+        private System.Windows.Forms.Button randomizer;
+        private System.Windows.Forms.TextBox dobPicker;
+        private System.Windows.Forms.TextBox favNum;
+        private System.Windows.Forms.RadioButton easybutton;
+        private System.Windows.Forms.RadioButton mediumbutton;
+        private System.Windows.Forms.RadioButton hardbutton;
+        private System.Windows.Forms.TextBox favNumBox;
+    }
+}
+
---
+++ UiPathDemoDesktopApp/MainForm.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UiPathDemoDesktopApp
+{
+    public partial class MainForm : Form
+    {
+        private int randomMode = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitbtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(fnameBox.Text))
+            {
+                MessageBox.Show("First name is required!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(lnameBox.Text))
+            {
+                MessageBox.Show("First name is required!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(pwBox.Text) )
+            {
+                MessageBox.Show("Password is required!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(cpwBox.Text))
+            {
+                MessageBox.Show("Password is required!");
+                return;
+            }
+            if (!pwBox.Text.Equals(cpwBox.Text))
+            {
+                MessageBox.Show("Passwords must be the same!");
+                return;
+            }
+            if (!tosBox.Checked)
+            {
+                MessageBox.Show("You must accept our TOS!");
+            }
+
+            MessageBox.Show("Welcome " + fnameBox.Text + "!");
+
+            fnameBox.Clear();
+            lnameBox.Clear();
+            pwBox.Clear();
+            cpwBox.Clear();
+            dobPicker.Clear();
+            favNumBox.Clear();
+            spamBox.Checked = true;
+            tosBox.Checked = false;
+        }
+
+        private void randomizer_Click(object sender, EventArgs e)
+        {
+            if (randomMode >= 1)
+            {
+                applyToAllControls(x => SetRandomLocation(x));
+            }
+            if (randomMode >= 2)
+            {
+                applyToAllControls(x => x.Scale(GetRandomScale()));
+            }
+            if (randomMode >= 3)
+            {
+                addEventHandlerToAllControls(new EventHandler(RandomLocationEventHandler));
+            }
+        }
+
+        private void RandomLocationEventHandler(object sender, EventArgs e)
+        {
+            var control = (Control)sender;
+            SetRandomLocation(control);
+        }
+
+        private void SetRandomLocation(Control control)
+        {
+            control.Location = GetRandomPoint();
+        }
+
+        private void slideDvd(Control control)
+        {
+            new Task(() =>
+            {
+                slideToDestination(GetRandomPoint(), control, 25, delegate() { Thread.Sleep(5); slideDvd(control); });
+            }).Start();
+        }
+        private void rainbow(Control control)
+        {
+            new Task(() =>
+            {
+                colorize(control, 100, delegate () { rainbow(control); });
+            }).Start();
+        }
+
+        private void applyToAllControls(Action<Control> action)
+        {
+            var typeToBeSelected = new List<Type>
+                                    {
+                                        typeof(TextBox)
+                                        , typeof(MaskedTextBox)
+                                        , typeof(Button)
+                                        , typeof(RadioButton)
+                                        , typeof(Label)
+                                        , typeof(DateTimePicker)
+                                        , typeof(CheckBox)
+                                    };
+
+            foreach (var item in GetAll(TopLevelControl, typeToBeSelected))
+            {
+                action.Invoke(item);
+            }
+        }
+        private void addEventHandlerToAllControls(EventHandler e)
+        {
+            applyToAllControls(x => x.TextChanged += e);
+        }
+
+        private IEnumerable<Control> GetAll(Control control, IEnumerable<Type> filteringTypes)
+        {
+            var ctrls = control.Controls.Cast<Control>();
+
+            return ctrls.SelectMany(ctrl => GetAll(ctrl, filteringTypes))
+                        .Concat(ctrls)
+                        .Where(ctl => filteringTypes.Any(t => ctl.GetType() == t));
+        }
+
+        private void slideToDestination(Point destination, Control control, int delay, Action onFinish)
+        {
+            new Task(() =>
+            {
+                int directionX = destination.X > control.Left ? 1 : -1;
+                int directionY = destination.Y > control.Top ? 1 : -1;
+
+                while (control.Left != destination.X || control.Top != destination.Y)
+                {
+                    try
+                    {
+                        if (control.Left != destination.X)
+                        {
+                            this.Invoke((Action)delegate ()
+                            {
+                                control.Left += directionX;
+                            });
+                        }
+                        if (control.Top != destination.Y)
+                        {
+                            this.Invoke((Action)delegate ()
+                            {
+                                control.Top += directionY;
+                            });
+                        }
+                        Thread.Sleep(delay);
+                    }
+                    catch
+                    {
+                        // form could be disposed
+                        break;
+                    }
+                }
+
+                if (onFinish != null) onFinish();
+
+            }).Start();
+        }
+
+        private void colorize(Control control, int delay, Action onFinish)
+        {
+            new Task(() =>
+            {
+                var ran = new Random();
+                var foreColor = Color.FromArgb(ran.Next(0, 255), ran.Next(0, 255), ran.Next(0, 255));
+                var dirR = foreColor.R > control.ForeColor.R ? 1 : -1;
+                var dirG = foreColor.G > control.ForeColor.G ? 1 : -1;
+                var dirB = foreColor.B > control.ForeColor.B ? 1 : -1;
+                while (control.ForeColor != foreColor)
+                {
+                    try
+                    {
+                        this.Invoke((Action)delegate ()
+                        {
+                            control.ForeColor = Color.FromArgb(control.ForeColor.R != foreColor.R ? control.ForeColor.R + dirR : control.ForeColor.R, control.ForeColor.G != foreColor.G ? control.ForeColor.G + dirG : control.ForeColor.G, control.ForeColor.B != foreColor.B ? control.ForeColor.B + dirB : control.ForeColor.B);
+                        });
+                        Thread.Sleep(delay);
+                    }
+                    catch
+                    {
+                        // form could be disposed
+                        break;
+                    }
+                }
+
+                if (onFinish != null) onFinish();
+
+            }).Start();
+        }
+
+        private Point GetRandomPoint()
+        {
+            var rng = new Random();
+            return new Point(rng.Next(0, Width), rng.Next(0, Height));
+        }
+
+        private SizeF GetRandomScale()
+        {
+            var rng = new Random();
+            var x = (float)rng.NextDouble() * 2;
+            var y = (float)rng.NextDouble() * 2;
+            return new SizeF(x,y);
+        }
+
+        private void easybutton_CheckedChanged(object sender, EventArgs e)
+        {
+            randomMode = easybutton.Checked ? 1 : 0;
+            if (easybutton.Checked)
+            {
+                mediumbutton.Checked = false;
+                hardbutton.Checked = false;
+            }
+        }
+
+        private void mediumbutton_CheckedChanged(object sender, EventArgs e)
+        {
+            randomMode = mediumbutton.Checked ? 2 : 0;
+            if (mediumbutton.Checked)
+            {
+                easybutton.Checked = false;
+                hardbutton.Checked = false;
+            }
+        }
+
+        private void hardbutton_CheckedChanged(object sender, EventArgs e)
+        {
+            randomMode = hardbutton.Checked ? 3 : 0;
+            if (hardbutton.Checked)
+            {
+                easybutton.Checked = false;
+                mediumbutton.Checked = false;
+            }
+        }
+        
+    }
+}
